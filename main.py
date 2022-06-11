36729f0d5ba0f4ec8ca02df6a749229dcb62dfdb
unknown <david22294@gmail.com>
2022-06-11 15:01:07 +0800

    Modify for new website
    --amend
    
    Fix bug
    
    fix but
    
    fix bug
    
    fix bug

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ from bs4 import BeautifulSoup
 from lxml import etree
 import time
 import pandas
-import datetime 
+import datetime
+import re
 
+doneList = set()
 
 columnName = ['department', 
               'tenderNum', 
@@ -33,34 +35,36 @@ outputColumn = dict()
 for name in columnName:
     outputColumn[chineseColumnName[name]] = []
 
-def createPostData(orgId, StartTime, EndTime):
-    postForm = {'method':'search',
-     'searchMethod':'true',
-     'orgId':orgId,
-     'hid_1':'1',
-     'tenderType':'tenderDeclaration',
-     'tenderWay': '1,2,3,4,5,6,7,10,12',
-     'tenderDateRadio':'on',
-     'tenderStartDateStr': StartTime,
-     'tenderEndDateStr': EndTime,
-     'tenderStartDate': StartTime,
-     'tenderEndDate': EndTime,
-     'isSpdt':'N',
-     'btnQuery':'查詢'
+def createPostData(orgId, departmentName, StartTime, EndTime):
+    postForm = {
+        'pageSize':'',
+        'firstSearch':'true',
+        'searchType':'basic',
+        'level_1':'on',
+        'orgName':departmentName,
+        'orgId':orgId,
+        'tenderName':'',
+        'tenderId':'',
+        'tenderType':'TENDER_DECLARATION',
+        'tenderWay':'TENDER_WAY_ALL_DECLARATION',
+        'dateType':'isDate',
+        'tenderStartDate':StartTime,
+        'tenderEndDate':EndTime,
+        'radProctrgCate':''
     }
     return postForm
 
 def getColumnInf(column, var):
     return {
-        'department' : column[1].text,
+        'department' : column[1].text.replace('\t','').replace('\n','').replace('\r','').replace(' ',''),
         'tenderNum' : column[2].text.split("\r\n")[1].replace('\t',''),
-        'tenderName' : column[2].find("a").get("title"),
-        'tenderMethod' : column[4].text.replace('\t','').replace('\n','').replace('\r',''),
-        'tenderType' : column[5].text.replace('\t','').replace('\n','').replace('\r',''),
-        'annouceDate' : column[6].text.replace('\t','').replace('\n','').replace('\r',''),
-        'deadline' : column[7].text.replace('\t','').replace('\n','').replace('\r',''),
-        'money' : column[8].text.replace('\t','').replace('\n','').replace('\r',''),
-        'trendHref' : "https://web.pcc.gov.tw/tps{i}\n".format(i=column[2].find("a").get("href")[2:]),
+        'tenderName' : re.findall('\("([^"]+)"\)', column[2].find("a").find("u").find("span").string)[0],
+        'tenderMethod' : column[4].text.replace('\t','').replace('\n','').replace('\r','').replace(' ',''),
+        'tenderType' : column[5].text.replace('\t','').replace('\n','').replace('\r','').replace(' ',''),
+        'annouceDate' : column[6].text.replace('\t','').replace('\n','').replace('\r','').replace(' ',''),
+        'deadline' : column[7].text.replace('\t','').replace('\n','').replace('\r','').replace(' ',''),
+        'money' : column[8].text.replace('\t','').replace('\n','').replace('\r','').replace(' ',''),
+        'trendHref' : "https://web.pcc.gov.tw{i}\n".format(i=column[2].find("a").get("href")),
     }.get(var,'error')
 
 def getException():
@@ -78,8 +82,8 @@ def getException():
         exceptionList['tenderType'] = []
     return exceptionList
 
-basePath = "https://web.pcc.gov.tw/tps/main/pss/pblm/tender/basic/search/"
-buyPostPath = "https://web.pcc.gov.tw/tps/pss/tender.do?searchMode=common&searchType=basic"
+basePath = "https://web.pcc.gov.tw"
+buyPostPath = "https://web.pcc.gov.tw/prkms/tender/common/basic/readTenderBasic?"
 
 if __name__ == '__main__':
     
@@ -89,26 +93,24 @@ if __name__ == '__main__':
     
     currentTime = datetime.datetime.now()
     weekAgoTime = currentTime - datetime.timedelta(days = int(dayRange))
-    StartTime = "{Y}/{M}/{D}".format(Y=weekAgoTime.year-1911, M=weekAgoTime.month, D=weekAgoTime.day)
-    StartTime = "{Y}/{MD}".format(Y=weekAgoTime.year-1911, MD=weekAgoTime.strftime("%m/%d"))
-    EndTime = "{Y}/{MD}".format(Y=currentTime.year-1911, MD=currentTime.strftime("%m/%d"))
+    StartTime = "{Y}/{M}/{D}".format(Y=weekAgoTime.year, M=weekAgoTime.month, D=weekAgoTime.day)
+    StartTime = "{Y}/{MD}".format(Y=weekAgoTime.year, MD=weekAgoTime.strftime("%m/%d"))
+    EndTime = "{Y}/{MD}".format(Y=currentTime.year, MD=currentTime.strftime("%m/%d"))
     
     print("Start Time: "+StartTime)
     print("End Time: "+EndTime)
     
-    root = requests.get("https://web.pcc.gov.tw/tps/main/pss/pblm/tender/basic/search/mainListCommon.jsp?searchType=basic")
+    root = requests.get("https://web.pcc.gov.tw/prkms/tender/common/orgName/indexTenderOrgName")
     rootContent = root.content.decode()
     rootHtmltml = etree.HTML(rootContent)
     departments = rootHtmltml.xpath("//p/a[@style='color: blue;']")
     print(len(departments))
-#     x = 0
     for department in departments:
-#         x = x + 1
-#         if (x == 5): break;
         print(department.attrib['title'])
         if department.attrib['title'] == '各級學校':
             print('Stop at 各級學校')
             break
+        
         targetPath = basePath + department.attrib['href']
         targetRoot = requests.get(targetPath)
         targetSoup = BeautifulSoup(targetRoot.text, "html.parser")
@@ -120,17 +122,19 @@ if __name__ == '__main__':
             else:
                 orgId = itemColumn[0].string
                 departmentName = itemColumn[1].string
-                postData = createPostData(orgId, StartTime, EndTime)
+                postData = createPostData(orgId, departmentName, StartTime, EndTime)
                 buy = requests.post(buyPostPath, data=postData)
-                time.sleep(2) # 
+                time.sleep(1) # 
 
                 buySoup = BeautifulSoup(buy.text, "html.parser")
-                items = buySoup.find_all("tr", onmouseover="overcss(this);")
+                items = buySoup.find("table", attrs={'class':'tb_01'}).find("tbody").findChildren("tr")
                 column = None
                 if orgId == None:
                     print('\t'+departmentName+' '+str(orgId)+' => None')
-                elif len(items)<1:
+                elif len(items)<2:
                     print('\t'+departmentName+' '+str(orgId)+' => None')
+                elif orgId in doneList:
+                    print('\t'+departmentName+' '+str(orgId)+' => Done Before')
                 else:
                     print('\t'+departmentName+' '+str(orgId)+' => '+str(len(items)))
                     
@@ -139,19 +143,24 @@ if __name__ == '__main__':
                         isPass = False
                         infs = []
                         
-                        # get tend inf
+                        # get trend inf
                         column = item.find_all("td")
-                        for name in columnName:
-                            if not isPass:
-                                var = getColumnInf(column, name)
-                                infs.append(var)
-                                if name in exceptionList.keys():
-                                    if var in exceptionList[name]:
-                                        isPass = True
+                        try:
+                            for name in columnName:
+                                if not isPass:
+                                    var = getColumnInf(column, name)
+                                    infs.append(var)
+                                    if name in exceptionList.keys():
+                                        if var in exceptionList[name]:
+                                            isPass = True
+                        except:
+                            print("Some Error!\n")
                         
                         if not isPass:
                             for i, name in enumerate(columnName):
                                 outputColumn[chineseColumnName[name]].append(infs[i])
+                    doneList.add(orgId)
+
     df = pandas.DataFrame(outputColumn)
     df.to_excel("國家機關採購案.xlsx",index=False, encoding='utf_8_sig')               
                         
